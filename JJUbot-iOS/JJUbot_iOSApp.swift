0fc4907eefecea8effd3449d8fb9287f5be4643f
zzung00 <lee759444@gmail.com>
2022-03-24 11:10:12 +0900

    Initial Commit

---
+++ JJUbot-iOS/JJUbot_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JJUbot_iOSApp.swift
+//  JJUbot-iOS
+//
+//  Created by 이정현 on 2022/03/24.
+//
+
+import SwiftUI
+
+@main
+struct JJUbot_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
