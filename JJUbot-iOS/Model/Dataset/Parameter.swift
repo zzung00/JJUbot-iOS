eb8c21cac4e13377ef4c289ed061238b12a9918a
zzung00 <lee759444@gmail.com>
2022-03-25 22:42:03 +0900

    create parameter in dataset for xml parsing

---
+++ JJUbot-iOS/Model/Dataset/Parameter.swift
@@ -0,0 +1,17 @@
+//
+//  Parameter.swift
+//  JJUbot-iOS
+//
+//  Created by 이정현 on 2022/03/25.
+//
+
+import Foundation
+
+struct Parameters {
+    var parameter: [Parameter]
+}
+
+struct Parameter {
+    var id: String
+    var value: String
+}
