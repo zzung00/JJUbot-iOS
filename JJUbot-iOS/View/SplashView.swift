47c709e3e3562938d91eb2f9818e7d601209782e
zzung00 <lee759444@gmail.com>
2022-03-26 15:36:10 +0900

    realize view movement

--- JJUbot-iOS/View/SplashView.swift
+++ JJUbot-iOS/View/SplashView.swift
@@ -8,27 +8,43 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State var isActive: Bool = false
+    
     var body: some View {
-        HStack {
-            Text("JJU")
-                .font(Font.custom("JalnanOTF", size: 50))
-                .bold()
-                .foregroundColor(.mainBlue)
-                .position(x: 170, y: 300)
-            HStack {
-                Text("Bot")
-                    .font(Font.custom("JalnanOTF", size: 50))
-                    .bold()
-                    .foregroundColor(.mainBlue)
-                    .position(x: 70, y: 350)
+        VStack {
+            if self.isActive == true {
+                SignInView()
             }
-            HStack {
-                Image("instarLogo")
-                    .resizable()
-                    .renderingMode(.template)
-                    .foregroundColor(.mainYellow)
-                    .frame(width: 70, height: 70)
-                    .position(x: -10, y: 300)
+            else {
+                HStack {
+                    Text("JJU")
+                        .font(Font.custom("JalnanOTF", size: 50))
+                        .bold()
+                        .foregroundColor(.mainBlue)
+                        .position(x: 170, y: 300)
+                    HStack {
+                        Text("Bot")
+                            .font(Font.custom("JalnanOTF", size: 50))
+                            .bold()
+                            .foregroundColor(.mainBlue)
+                            .position(x: 70, y: 350)
+                    }
+                    HStack {
+                        Image("instarLogo")
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundColor(.mainYellow)
+                            .frame(width: 70, height: 70)
+                            .position(x: -10, y: 300)
+                    }
+                }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                        withAnimation {
+                            self.isActive = true
+                        }
+                    }
+                }
             }
         }
     }
