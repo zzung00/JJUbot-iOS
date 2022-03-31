6f5fd99a24d10f24bc17d49cc3ee067157d0cb75
zzung00 <lee759444@gmail.com>
2022-03-31 18:00:49 +0900

    create Sending View

---
+++ JJUbot-iOS/View/SendingView.swift
@@ -0,0 +1,20 @@
+//
+//  SendingView.swift
+//  JJUbot-iOS
+//
+//  Created by 이정현 on 2022/03/31.
+//
+
+import SwiftUI
+
+struct SendingView: View {
+    var body: some View {
+        Text("hi")
+    }
+}
+
+struct SendingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SendingView()
+    }
+}
