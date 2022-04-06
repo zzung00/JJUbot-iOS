402fa0e710fcce23e83e8b0445ad9356e8e54820
zzung00 <lee759444@gmail.com>
2022-04-06 20:26:41 +0900

    added SendingView interface

--- JJUbot-iOS/View/SendingView.swift
+++ JJUbot-iOS/View/SendingView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct SendingView: View {
+    @State private var destination = ""
+    @State private var reciever = ""
+    
     var body: some View {
-        Text("hi")
+        VStack {
+            ZStack {
+                Section {
+                    VStack {
+                    Text("목적지를 입력하세요")
+                        .font(Font.custom("JalnanOTF", size: 25))
+                    
+                    TextField("목적지", text: $destination)
+                        .frame(width: 350, height: 100)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.default)
+                        .disableAutocorrection(true)
+                    
+                    Text("수신자 학번을 입력하세요")
+                        .font(Font.custom("JalnanOTF", size: 25))
+                    
+                    TextField("수신자 학번", text: $reciever)
+                        .frame(width: 350, height: 100)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.default)
+                        .disableAutocorrection(true)
+                    }
+                }
+            }
+        }
     }
 }
 
