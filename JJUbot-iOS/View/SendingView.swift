ee0c65825bb106eea8e90d11c6562b47bcb3b1f8
zzung00 <lee759444@gmail.com>
2022-05-19 19:54:56 +0900

    update

--- JJUbot-iOS/View/SendingView.swift
+++ JJUbot-iOS/View/SendingView.swift
@@ -10,31 +10,29 @@ import SwiftUI
 struct SendingView: View {
     @State private var destination = ""
     @State private var reciever = ""
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
-        VStack {
-            ZStack {
-                Section {
-                    VStack {
-                    Text("목적지를 입력하세요")
-                        .font(Font.custom("JalnanOTF", size: 25))
-                    
-                    TextField("목적지", text: $destination)
-                        .frame(width: 350, height: 100)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.default)
-                        .disableAutocorrection(true)
-                    
-                    Text("수신자 학번을 입력하세요")
-                        .font(Font.custom("JalnanOTF", size: 25))
-                    
-                    TextField("수신자 학번", text: $reciever)
-                        .frame(width: 350, height: 100)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.default)
-                        .disableAutocorrection(true)
-                    }
+        ZStack(alignment: .topLeading) {
+            Color.lightGray
+                .ignoresSafeArea()
+            
+            VStack {
+                Button(action: {self.presentationMode.wrappedValue.dismiss()}) {
+                    Image(systemName: "xmark")
+                        .font(.title)
+                        .foregroundColor(.red)
+                        .padding(20)
                 }
+                Spacer()
+            }
+            
+            VStack(alignment: .leading) {
+                Text("배송 정보를 정확하게 입력해주세요")
+                    .foregroundColor(.black)
+                    .font(Font.custom("JalnanOTF", size: 17))
+                    .frame(width: 300)
+                    .position(x: 150, y: 80)
             }
         }
     }
@@ -45,3 +43,19 @@ struct SendingView_Previews: PreviewProvider {
         SendingView()
     }
 }
+
+
+//                            TextField("목적지", text: $destination)
+//                                .frame(width: 350, height: 100)
+//                                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                                .keyboardType(.default)
+//                                .disableAutocorrection(true)
+//
+//                            Text("수신자 학번을 입력하세요")
+//                                .font(Font.custom("JalnanOTF", size: 25))
+//
+//                            TextField("수신자 학번", text: $reciever)
+//                                .frame(width: 350, height: 100)
+//                                .textFieldStyle(RoundedBorderTextFieldStyle())
+//                                .keyboardType(.default)
+//                                .disableAutocorrection(true)
