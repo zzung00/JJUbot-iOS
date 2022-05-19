78f8b8614668b6750ab6b6945872194d4157115a
zzung00 <lee759444@gmail.com>
2022-05-19 15:58:00 +0900

    added cardModifier for cardView

---
+++ JJUbot-iOS/ViewModel/CardModifier.swift
@@ -0,0 +1,17 @@
+//
+//  CardModifier.swift
+//  JJUbot-iOS
+//
+//  Created by 이정현 on 2022/05/19.
+//
+
+import Foundation
+import SwiftUI
+
+struct CardModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .cornerRadius(20)
+            .shadow(color: Color.black.opacity(0.5), radius: 20, x: 0, y: 0)
+    }
+}
