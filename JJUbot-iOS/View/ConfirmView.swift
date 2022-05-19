aece871b3318334e87b01decd5119a0a471edfce
zzung00 <lee759444@gmail.com>
2022-05-19 16:13:25 +0900

    added view that confirm for sending information

---
+++ JJUbot-iOS/View/ConfirmView.swift
@@ -0,0 +1,22 @@
+//
+//  ConfirmView.swift
+//  JJUbot-iOS
+//
+//  Created by 이정현 on 2022/05/18.
+//
+
+import SwiftUI
+
+struct ConfirmView: View {
+    var body: some View {
+        NavigationView {
+                Text("Hello, World!")
+        }
+    }
+}
+
+struct ConfirmView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfirmView()
+    }
+}
