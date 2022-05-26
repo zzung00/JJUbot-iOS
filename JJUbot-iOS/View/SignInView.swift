1b12bc3d8201f6b2d44f90eb48e576011b48136b
zzung00 <lee759444@gmail.com>
2022-05-26 23:30:44 +0900

    Feat: Move view according to parsing response

--- JJUbot-iOS/View/SignInView.swift
+++ JJUbot-iOS/View/SignInView.swift
@@ -21,38 +21,42 @@ struct SignInView: View {
     @StateObject private var viewModel = SignInViewModel()
     
     var body: some View {
-        VStack(alignment: .center, spacing: 60) {
-            Image("jjLogo")
-            
-            Text("로그인 하세요")
-                .bold()
-                .font(Font.custom("JalnanOTF", size: 30))
-                .foregroundColor(.mainBlue)
-            
-            VStack {
-                TextField("학번을 입력하세요", text: $username)
-                    .keyboardType(.decimalPad)
-                    .frame(width: 300, height: 40, alignment: .center)
-                    .cornerRadius(4.0)
-                    .textFieldStyle(.roundedBorder)
+        if viewModel.isSuccess {
+            MainView()
+        }else {
+            VStack(alignment: .center, spacing: 60) {
+                Image("jjLogo")
                 
-                SecureField("비밀번호를 입력하세요", text: $password)
-                    .keyboardType(.default)
-                    .frame(width: 300, height: 40, alignment: .center)
-                    .cornerRadius(4.0)
-                    .textFieldStyle(.roundedBorder)
-                    .textInputAutocapitalization(.never)
-            }
-            
-            VStack {
-                Button(action: {viewModel.SignInRequest(username: username, password: password)}) {
-                    Text("로그인")
-                        .font(Font.custom("JalnanOTF", size: 20))
-                        .foregroundColor(.mainYellow)
+                Text("로그인 하세요")
+                    .bold()
+                    .font(Font.custom("JalnanOTF", size: 30))
+                    .foregroundColor(.mainBlue)
+                
+                VStack {
+                    TextField("학번을 입력하세요", text: $username)
+                        .keyboardType(.decimalPad)
+                        .frame(width: 300, height: 40, alignment: .center)
+                        .cornerRadius(4.0)
+                        .textFieldStyle(.roundedBorder)
+                    
+                    SecureField("비밀번호를 입력하세요", text: $password)
+                        .keyboardType(.default)
+                        .frame(width: 300, height: 40, alignment: .center)
+                        .cornerRadius(4.0)
+                        .textFieldStyle(.roundedBorder)
+                        .textInputAutocapitalization(.never)
                 }
-                .frame(width: 200, height: 45, alignment: .center)
-                .background(Color.mainBlue)
-                .cornerRadius(6.0)
+                
+                VStack {
+                    Button(action: {viewModel.SignInRequest(username: username, password: password)}) {
+                        Text("로그인")
+                            .font(Font.custom("JalnanOTF", size: 20))
+                            .foregroundColor(.mainYellow)
+                    }
+                    .frame(width: 200, height: 45, alignment: .center)
+                    .background(Color.mainBlue)
+                    .cornerRadius(6.0)
+                }
             }
         }
     }
