8d82d4e7da234a42d8a6a4a61c76abe7e84823bb
zzung00 <lee759444@gmail.com>
2022-05-28 00:04:38 +0900

    Feat: added parameter to notify login failure

--- JJUbot-iOS/ViewModel/SignInViewModel.swift
+++ JJUbot-iOS/ViewModel/SignInViewModel.swift
@@ -10,12 +10,14 @@ import Foundation
 class SignInViewModel: ObservableObject {
     @Published var name: String? = ""
     @Published var isSuccess: Bool = false
+    @Published var showToast = false
     
     func SignInRequest(username: String, password: String) {
         JJUbotService.shared.signInJJInstar(username: username, password: password) {
             result in
             if result == nil {
                 self.isSuccess = false
+                self.showToast = true
             } else {
                 self.isSuccess = true
                 self.name = result
