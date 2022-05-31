b75a5541c50e5f83c67d06313471a2ad8a936cf2
zzung00 <lee759444@gmail.com>
2022-05-31 16:36:41 +0900

    Refactor: Use sheet to move view

--- JJUbot-iOS/View/MainView.swift
+++ JJUbot-iOS/View/MainView.swift
@@ -18,45 +18,41 @@ struct MainView: View {
     
     var body: some View {
         NavigationView {
-            if isActive == true {
-                SendingView()
-            }
-            else {
-                VStack {
-                    Text("배달봇 찾기")
-                        .font(Font.custom("JalnanOTF", size: 20))
-                        .foregroundColor(.black)
-                        .navigationTitle("JJUbot")
-                        .navigationBarTitleDisplayMode(.inline)
+            VStack {
+                Text("배달봇 찾기")
+                    .font(Font.custom("JalnanOTF", size: 20))
+                    .foregroundColor(.black)
+                    .navigationTitle("JJUbot")
+                    .navigationBarTitleDisplayMode(.inline)
                     
-                    ZStack {
-                        Color.lightGray
-                            .edgesIgnoringSafeArea(.all)
-                        ScrollView(.vertical, showsIndicators: false) {
-                            VStack {
-                                ProductCard(image: "instarLogo", title: "JJUbot")
-                                    .frame(alignment: .center)
-                            }
+                ZStack {
+                    Color.lightGray
+                        .edgesIgnoringSafeArea(.all)
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack {
+                            ProductCard(image: "instarLogo", title: "JJUbot")
+                                .frame(alignment: .center)
                         }
                     }
+                }
                         
-                    VStack {
-                        //배달봇 미선택하고 호출 버튼만 누를 경우, 선택하라는 경고 창 표시
-                        //다이얼로그 버튼마다 액션 추가 예정
-                        Button(action: {self.showAlert = true}) {
-                            Text("배달봇 호출")
-                                .frame(maxWidth: .infinity)
-                                .foregroundColor(.mainYellow)
-                                .padding()
-                                .font(Font.custom("JalnanOTF", size: 30))
-                        }
-                        .background(Color.mainBlue)
-                        .alert(isPresented: $showAlert) {
-                        Alert(title: Text("JJUbot-Service"), message: Text("배달봇을 호출하시겠습니까?"), primaryButton: .destructive(Text("예")) {
-                            self.isActive = true
-                            viewModel.callJJUbot()
-                            }, secondaryButton: .cancel(Text("아니오")))
-                        }
+                VStack {
+                    Button(action: {self.showAlert = true}) {
+                        Text("배달봇 호출")
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(.mainYellow)
+                            .padding()
+                            .font(Font.custom("JalnanOTF", size: 30))
+                    }
+                    .background(Color.mainBlue)
+                    .alert(isPresented: $showAlert) {
+                    Alert(title: Text("JJUbot-Service"), message: Text("배달봇을 호출하시겠습니까?"), primaryButton: .destructive(Text("예")) {
+                        self.isActive = true
+                        viewModel.callJJUbot()
+                        }, secondaryButton: .cancel(Text("아니오")))
+                    }
+                    .sheet(isPresented: $isActive) {
+                        SendingView()
                     }
                 }
             }
