0fd464ef9ba653f26dbf0de16e790a9981fc36d5
zzung00 <lee759444@gmail.com>
2022-06-11 16:22:41 +0900

    Feat: Create ConfirmView

--- JJUbot-iOS/View/ConfirmView.swift
+++ JJUbot-iOS/View/ConfirmView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ConfirmView: View {
     var body: some View {
-        NavigationView {
-                Text("Hello, World!")
+        ZStack {
+            VStack(alignment: .center, spacing: 5) {
+                Image(systemName: "checkmark.circle.fill")
+                    .resizable()
+                    .foregroundColor(.mainBlue)
+                    .frame(width: 150, height: 150)
+                    .padding(50)
+                        
+                Text("배달봇이 성공적으로 호출됐어요!")
+                    .foregroundColor(.black)
+                    .font(Font.custom("JalnanOTF", size: 20))
+                    .frame(width: 300)
+            }
+            .padding([.bottom], 250)
+            Spacer()
         }
     }
 }
